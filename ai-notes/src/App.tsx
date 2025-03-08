3a1b8abfffba0da399416dfc4aa041e39f076cd0
Larue <laruelinder77@gmail.com>
2025-03-08 15:36:21 -0500

    working

--- ai-notes/src/App.tsx
+++ ai-notes/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showMessage, setShowMessage] = useState(false);
+  const message = useQuery(api.hello.greet, {
+    name: " world",
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      
+      {showMessage && (
+        <div className="m-2">
+          Backend says:{" "}
+          <code className="border px-2 py-1 rounded-md text-sm">{message}</code>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
