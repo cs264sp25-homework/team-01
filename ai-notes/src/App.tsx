bfedd41c180eb0ce2a81c6aabac59201f7973174
Larue <laruelinder77@gmail.com>
2025-03-08 15:46:38 -0500

    tailwind error

--- ai-notes/src/App.tsx
+++ ai-notes/src/App.tsx
@@ -3,20 +3,16 @@ import { api } from "../convex/_generated/api";
 import { useState } from "react";
 
 function App() {
-  const [showMessage, setShowMessage] = useState(false);
   const message = useQuery(api.hello.greet, {
     name: " world",
   });
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      
-      {showMessage && (
-        <div className="m-2">
-          Backend says:{" "}
-          <code className="border px-2 py-1 rounded-md text-sm">{message}</code>
-        </div>
-      )}
+      <div className="m-2">
+        Backend says:{" "}
+        <code className="border px-2 py-1 rounded-md text-sm">{message}</code>
+      </div>
     </div>
   );
 }
