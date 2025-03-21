28208bb03faf22748747488b58e976b2ddee41f8
Larue <laruelinder77@gmail.com>
2025-03-21 18:30:42 -0400

    imported tailwind

---
+++ src/editor/page.tsx
@@ -0,0 +1,12 @@
+import { usePlateEditor, Plate, PlateContent } from "@udecode/plate/react";
+
+export default function BasicEditor() {
+  const editor = usePlateEditor();
+
+  return (
+    <Plate editor={editor}>
+      <PlateContent placeholder="Type..." />
+    </Plate>
+  );
+}
+
