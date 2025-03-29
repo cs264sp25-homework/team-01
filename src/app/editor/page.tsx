add4f4fa32e7753206d2df8cdc61054a044d86a3
Taran Agarwal <tagarwa4@jh.edu>
2025-03-28 22:18:46 -0400

    Added routing for sign in

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -2,7 +2,7 @@ import { PlateEditor } from '@/components/editor/plate-editor';
 
 export default function Page() {
   return (
-    <div className="h-screen w-full" data-registry="plate">
+    <div className="w-full h-screen" data-registry="plate">
       <PlateEditor />
     </div>
   );
