69d29db7064830be37de81cb8005036d4ceeda55
justinb91011 <justin_danielbravo@icloud.com>
2025-04-06 14:24:16 -0400

    Should build app now

--- src/components/plate-ui/blockquote-element-static.tsx
+++ src/components/plate-ui/blockquote-element-static.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 
 import type { SlateElementProps } from '@udecode/plate';
 
--- src/components/plate-ui/code-block-element-static.tsx
+++ src/components/plate-ui/code-block-element-static.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 
 import type { SlateElementProps } from '@udecode/plate';
 import type { TCodeBlockElement } from '@udecode/plate-code-block';
